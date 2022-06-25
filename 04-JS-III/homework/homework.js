babc2b6424a089f57c7b0dd85b9f7d24010020d2
SantiagoPaguaga <snpaguaga@gmail.com>
2022-06-24 22:51:43 -0300

    Second Commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -7,7 +7,7 @@ function devolverPrimerElemento(array) {
 
 
 function devolverUltimoElemento(array) {
-  // Devuelve el último elemento de un array
+  // Devuelveee el último elemento de un array
   // Tu código:
 }
 
